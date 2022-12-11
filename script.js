72fdce1b117964c7ed554759f1bd7f9c531d9cee
catiacfsilva <115549806+catiacfsilva@users.noreply.github.com>
2022-12-11 20:45:41 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let name = "Cátia";
+console.log(name)
+
+let age = 19;
+console.log(age)
+
+let person = {
+    name: "Cátia",
+    age: 19,
+    isStudent: true
+};
+console.log(person.name);
+// Cátia
+
+console.log(7 * age)
+
+let = friends ["Beatriz", "Joana", "Natália"];
+let numbers = [1, 2, 3];
+console.log(friends[0]);
+
+let friends = [
+    {
+      name: "Beatriz",
+      age: 19,
+      isStudent: true
+    },
+    {
+      name: "Joana",
+      age: 19,
+      isStudent: true
+    },
+    {
+      name: "Natália",
+      age: 19,
+      isStudent: true
+    }
+  ];
+  console.log(friends[0].name);
+
